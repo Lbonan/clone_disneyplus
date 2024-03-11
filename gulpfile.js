b84cfbc48136087cb1f689337eaba896700cf34b
Lucas Bonan <lucasbonan15@gmail.com>
2024-03-11 20:53:08 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imageMin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+function scripts() {
+  return gulp.src("./src/scripts/*.js").pipe(uglify()).pipe(gulp.dest("./dist/js"));
+}
 
 function styles() {
   return gulp
@@ -13,7 +18,8 @@ function images() {
   return gulp.src("./src/images/**/*").pipe(imageMin()).pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function () {
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(scripts));
 };
